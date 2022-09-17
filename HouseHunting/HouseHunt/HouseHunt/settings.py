c9d45086b7fffe91bfe784efc23ff002395cb876
gamingflexer <osurve20ece@student.mes.ac.in>
2022-09-18 01:46:03 +0530

    settings change

--- HouseHunting/HouseHunt/HouseHunt/settings.py
+++ HouseHunting/HouseHunt/HouseHunt/settings.py
@@ -51,11 +51,13 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'HouseHunt.urls'
+LOGIN_REDIRECT_URL = "/"
+
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
